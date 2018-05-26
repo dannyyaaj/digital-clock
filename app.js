476a2aa4612f5869aea9739138e224f6e4e240c8
Danny Yang <dannyyaaj@gmail.com>
2018-05-26 14:05:20 -0500

    update app.js to es6 syntax by cleaning up logic for hours, mins, secs

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-
 function clock() {
 
   let fullDate = new Date();
   let hours = fullDate.getHours();
-  let mins = fullDate.getMinutes() + 3;
+  let mins = fullDate.getMinutes();
   let secs = fullDate.getSeconds();
-  let amPM = (hour > 11) ? "PM" : "AM";
-  
+  let amPM = hour >= 12 ? "AM" : "PM";
+
   if (hours > 12) {
     hours -= 12;
-  }
-  else if (hours === 0) {
+  } else if (hours === 0) {
     hours = 12;
-  }
-  else if (hours < 10) {
-    hours = "0" + hours;
+  } else if (hours < 10) {
+    hours = `0${hours}`;
   }
   if (mins < 10) {
-    mins = "0" + mins;
+    mins = `0${mins}`;
   }
   if (secs < 10) {
-    secs = "0" + secs;
+    secs = `0${secs}`;
   }
 
   document.getElementById("hour").innerHTML = hours;
-  document.getElementById("minute").innerHTML = ": " + mins;
-  document.getElementById("second").innerHTML = ": " + secs + " " + amPM;
-
+  document.getElementById("minute").innerHTML = `:${mins}`;
+  document.getElementById("second").innerHTML = `:${secs} ${amPM}`;
 };
 
-setInterval(clock,1000);
+setInterval(clock, 1000);
