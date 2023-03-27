a3119c22b6b6dc1254d0b96f3b62d71637e34833
Danny Yang <dannyyaaj@gmail.com>
2023-03-27 13:10:39 -0500

    refactor, add breakpoint for mobile view

--- app.js
+++ app.js
@@ -13,16 +13,19 @@ function clock() {
   } else if (hours < 10) {
     hours = `0${hours}`;
   }
+
   if (mins < 10) {
     mins = `0${mins}`;
   }
+
   if (secs < 10) {
     secs = `0${secs}`;
   }
 
-  document.getElementById("hour").innerHTML = hours;
-  document.getElementById("minute").innerHTML = `:${mins}`;
-  document.getElementById("second").innerHTML = `:${secs} ${amPM}`;
+  document.getElementById("hour").innerHTML = `${hours}:`
+  document.getElementById("minute").innerHTML = `${mins}:`
+  document.getElementById("second").innerHTML = secs
+  document.getElementById("period").innerHTML = amPM
 };
 
 setInterval(clock, 1000);
